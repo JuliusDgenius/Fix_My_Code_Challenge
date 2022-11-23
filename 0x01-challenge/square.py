6cf6ca8e1d64f35a16f80fb14abc9ec08b438dbf
JuliusDgenius <ibejulius1@gmail.com>
2022-11-23 18:57:41 +0100

    made corrections in square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+"""square module"""
+
 
 class square():
-    
+    """Defines s square"""    
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """initializes the square class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string rep of a Square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
